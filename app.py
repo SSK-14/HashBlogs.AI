c21f07e329bab62fb1fd079431cfe330d0bc3376
SSK-14 <sanjaykumar1481999@gmail.com>
2024-07-25 19:58:20 +0530

    add streamlit

---
+++ app.py
@@ -0,0 +1,9 @@
+import asyncio
+import streamlit as st
+
+async def main():
+    st.title("#️⃣ HashBlogs.:blue[AI]")
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="HashBlogs.AI", page_icon="✨")
+    asyncio.run(main())
